3215dc04780c255f969afe5a4960360845aba2bb
wolverinezgit <harshsrivastava9795@gmail.com>
2022-12-18 01:33:42 +0530

    database related and fixed bugs

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,29 @@ const cors=require("cors")
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+mongoose.connect("mongodb+srv://user:user123@cluster0.wrpgaob.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("DB connected")).catch((error)=>{console.log(error)});
 
+
+
+
+const schema=new mongoose.Schema({problemName:String,problemLink:String,tag:String,notes:String});
+const Model=mongoose.model('Model',schema);
 
 app.get("/",(req,res)=>{res.send("Hi")});
 app.post("/",(req,res)=>{
 
-    console.log(req.body);
+    const {problemName,problemLink,tag,notes}=req.body;
+    const newDocument=new Model({problemName,problemLink,tag,notes});
+    newDocument.save((err)=>{if(err)console.log(err)})
+const obj=req.body;
+// problemName: 'dwqdq',
+// problemLink: 'dwqwdqw',
+// tag: 'Dynammic Programming',
+// notes: 'dqwdqd'
+// }
+
+
+    console.log(problemLink,tag);
 });
 app.get("/delete",(req,res)=>{
 
